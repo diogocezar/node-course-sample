c48221b62ed2970c3378499f7baadac84adbf638
Diogo Cezar <diogo@diogocezar.com>
2019-06-07 19:54:58 -0300

    Adjustments

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb://node-api:123abc123@ds135207.mlab.com:35207/node-api",
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,6 +6,24 @@ class ProductController {
     const products = await Product.find();
     return res.json(products);
   }
+  async store(req, res) {
+    const product = await Product.create(req.body);
+    return res.json(product);
+  }
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
+  }
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    return res.json(product);
+  }
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+    return res.send({ deleted: true });
+  }
 }
 
 module.exports = new ProductController();
